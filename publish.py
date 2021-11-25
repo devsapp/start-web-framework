ce8f5e16455fd1b4e2f62fa0c5d3309a3e9a12cd
anycodes <service@52exe.cn>
2021-11-25 12:05:18 +0800

    update publish script

--- publish.py
+++ publish.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
+
 
 def getContent(fileList):
     for eveFile in fileList:
@@ -9,16 +11,24 @@ def getContent(fileList):
             pass
     return None
 
+
 with open('update.list') as f:
     publish_list = [eve_app.strip() for eve_app in f.readlines()]
 
 for eve_app in publish_list:
-    publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
-    command = 'cd %s && wget %s && python hub-publish.py' % (eve_app, publish_script)
-    child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,)
-    stdout, stderr = child.communicate()
-    if child.returncode == 0:
-        print("success:", stdout)
-    else:
-        print("error:", stderr)
-        raise ChildProcessError(stderr)
+    times = 0
+    while times <= 3:
+        print("----------------------: ", eve_app)
+        publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
+        command = 'cd %s && wget %s && python hub-publish.py' % (eve_app, publish_script)
+        child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
+        stdout, stderr = child.communicate()
+        if child.returncode == 0:
+            print("success:", stdout)
+            break
+        else:
+            print("error:", stderr)
+            time.sleep(3)
+            if times == 3:
+                raise ChildProcessError(stderr)
+        times = times + 1
