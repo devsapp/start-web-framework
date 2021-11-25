e9a9db04416f3aa16be2fd7615d42a93563ee343
anycodes <service@52exe.cn>
2021-11-25 11:09:48 +0800

    update publish script

--- publish.py
+++ publish.py
@@ -1,4 +1,4 @@
-import os
+import subprocess
 
 def getContent(fileList):
     for eveFile in fileList:
@@ -9,8 +9,16 @@ def getContent(fileList):
             pass
     return None
 
-with open('publish.cache') as f:
+with open('update.list') as f:
     publish_list = [eve_app.strip() for eve_app in f.readlines()]
 
 for eve_app in publish_list:
-    os.system('cd %s && wget https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py && python hub-publish.py' % eve_app)
+    publish_script = 'https://serverless-registry.oss-cn-hangzhou.aliyuncs.com/publish-file/python3/hub-publish.py'
+    command = 'cd %s && wget %s && python hub-publish.py' % (eve_app, publish_script)
+    child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,)
+    stdout, stderr = child.communicate()
+    if child.returncode == 0:
+        print("success:", stdout)
+    else:
+        print("error:", stderr)
+        raise ChildProcessError(stderr)
