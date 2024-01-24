c831f2aa8c9bba1f247c0cc8893dbd16f43a33c5
rsonghuster <ls_huster@163.com>
2024-01-24 22:44:55 +0800

    add start-tornado-v3

--- web-framework/python/tornado/src/code/index.py
+++ web-framework/python/tornado/src/code/index.py
@@ -2,9 +2,11 @@ import tornado.ioloop
 import tornado.web
 import os
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('''<html xmlns="http://www.w3.org/1999/xhtml">
+        self.write(
+            """<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Serverless Devs - Powered By Serverless Devs</title>
@@ -19,7 +21,7 @@ class MainHandler(tornado.web.RequestHandler):
         <br/>
         <p>您也可以快速体验： <br/>
             • 下载Serverless Devs工具：npm install @serverless-devs/s<br/>
-            • 初始化项目：s init start-tornado<br/>
+            • 初始化项目：s init start-tornado-v3<br/>
 
             • 项目部署：s deploy<br/>
             <br/>
@@ -29,12 +31,16 @@ class MainHandler(tornado.web.RequestHandler):
 </div>
 </body>
 </html>
-''')
+"""
+        )
+
 
 def make_app():
-    return tornado.web.Application([
-        (r"/.*", MainHandler),
-    ])
+    return tornado.web.Application(
+        [
+            (r"/.*", MainHandler),
+        ]
+    )
 
 
 if __name__ == "__main__":
