189186b2876fdcb8b02cefbf28771de80ecb746f
rsonghuster <ls_huster@163.com>
2024-03-18 20:18:19 +0800

    enhance github action

--- auto-publish.py
+++ auto-publish.py
@@ -6,20 +6,24 @@ os.system("s registry login --token {}".format(os.environ["REGISTRY_TOKEN"]))
 
 
 def search_publish_yaml(directory):
-    success_app_list = []
+    all_app_list = []
+    fail_app_list = []
     for root, dirs, files in os.walk(directory):
         if "publish.yaml" in files:
             d = os.path.join(os.getcwd(), root)
-            print("try publish dir = {}".format(d))
+            # print("try publish dir = {}".format(d))
+            all_app_list.append(d)
             try:
                 os.system("cd {} && s registry publish".format(d))
-                success_app_list.append(d)
             except Exception as e:
-                print(str(e))
+                print("publish {} error: {}".format(d, str(e)))
+                fail_app_list.append(d)
 
     print("\n\n*****************\n")
-    for item in success_app_list:
-        print("{} published success".format(item))
+    for item in all_app_list:
+        if item not in fail_app_list:
+            print("{} published success".format(item))
+    print("\n\n#################\n")
 
 
 # 在当前目录递归搜索
