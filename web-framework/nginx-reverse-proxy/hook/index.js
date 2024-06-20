57f1cc8511545647fa9939281079fe38422b2b76
灵轮 <linglun.zh@alibaba-inc.com>
2024-06-20 11:53:16 +0800

    Add nginx-reverse-proxy

---
+++ web-framework/nginx-reverse-proxy/hook/index.js
@@ -0,0 +1,26 @@
+async function preInit(inputObj) {
+    console.log(`
+  Serverless Devs Application Case
+    
+    Cloud services required：
+    - FC : https://fc.console.aliyun.com/
+    
+    Tips：
+    - Serverless Devs Version >= v2.0.103 
+    - FC Component: https://www.serverless-devs.com/fc/readme
+    `)
+}
+
+async function postInit(inputObj) {
+    console.log(`
+    * Before using, please check whether the actions command in Yaml file is available
+    * Carefully reading the notes in s.yaml is helpful for the use of the tool
+    * FC is only used as the execution environment, and the business code is in NAS
+    * If need help in the use process, please apply to join the Dingtalk Group: 33947367
+    `)
+}
+
+module.exports = {
+    postInit,
+    preInit
+}
---
+++ web-framework/nginx-reverse-proxy/src/code/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sed -i  's\${PROXY_PASS}\'$PROXY_PASS'\g' ./nginx.conf
+nginx -c /code/nginx.conf -g "daemon off;"
+
